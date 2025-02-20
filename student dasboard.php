52e697ead8f2e5909d0a4ac0b83bb77d46ae29c7
AkeelArshad <C2007010@hallam.shu.ac.uk>
2025-02-20 14:20:03 +0000

    Deleted student dashboard.php and students dashboard.html file

--- student dasboard.php
+++
@@ -1,9 +0,0 @@
-<!-- Student Dashboard -->
-<?php
-// student_dashboard.php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'student') {
-    header("Location: login.php");
-    exit();
-}
-?>
