8720e8b489827c1e85a076de5b9755e4314dbc11
JuniorBaldeh <c3021339@hallam.shu.ac.uk>
2025-02-20 14:25:35 +0000

    Add files via upload

---
+++ student dashboard.php
@@ -0,0 +1,9 @@
+<!-- Student Dashboard -->
+<?php
+// student_dashboard.php
+session_start();
+if (!isset($_SESSION['role']) || $_SESSION['role'] != 'student') {
+    header("Location: login.php");
+    exit();
+}
+?>
